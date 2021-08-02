ecc145563477ac5986a3e40caab0f57b592bfbdc
独行 <jsw0325@163.com>
2021-08-02 23:23:10 +0800

    feat: 添加所需图片和基础样式

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { NConfigProvider } from 'naive-ui'
 import { RouterView } from 'vue-router'
 import GlobalProvider from './components/GlobalProvider'
 import GloablInject from './components/GlobalProvider/GloablInject'
+import '@/styles/index.scss'
 
 export default defineComponent({
   name: 'App',
