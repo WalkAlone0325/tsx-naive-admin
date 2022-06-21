e4a3a89835940c5a95f7ec9be0e2f1feae1d728e
jsw <jsw0325@163.com>
2022-06-21 17:26:24 +0800

    feat: routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,12 +1,18 @@
 import type { RouteRecordRaw } from 'vue-router'
 import PageLayout from '@/layout/PageLayout'
 import appRoutes from './modules'
+import { HomeOutlined } from '@vicons/antd'
 
 export const constantRoutes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'root',
     component: PageLayout,
+    redirect: '/dashboard',
+    meta: {
+      icon: HomeOutlined,
+      title: '首页'
+    },
     children: appRoutes
   },
   {
