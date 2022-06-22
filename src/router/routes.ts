e6ddb8f5fa3e476b8efed631c269478409a6d36c
独行 <jsw0325@163.com>
2022-06-22 21:43:10 +0800

    fix: file name

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -9,13 +9,5 @@ export const constantRoutes: RouteRecordRaw[] = [
     component: PageLayout,
     redirect: '/home',
     children: appRoutes
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/AboutView.vue')
   }
 ]
